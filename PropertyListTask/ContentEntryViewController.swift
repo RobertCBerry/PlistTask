7a4d416749e1c3872fc71ceec012d2aa34e58ade
Robert Berry <robertcberry@yahoo.com>
2018-02-19 21:22:24 -0600

    Updated content entry view controller.

--- PropertyListTask/ContentEntryViewController.swift
+++ PropertyListTask/ContentEntryViewController.swift
@@ -12,17 +12,14 @@ class ContentEntryViewController: UIViewController {
     
     // MARK: Properties
     
-    // Retrieve the URL of the plist file.
+    @IBOutlet weak var showButton: UIBarButtonItem!
     
-    let plistUrl = Bundle.main.url(forResource: "Favorites", withExtension: "plist")
     
-    // Retrieve reference to the FileManager.
-    
-    let fileManager = FileManager.default
+    let plistManager = PlistManager()
     
     // favorite stores the data we read and allows us to append it.
     
-    var favorite: [String]?
+    var favorites: [String]?
     
     @IBOutlet weak var favoriteFoodTextView: UITextView!
     
@@ -32,40 +29,51 @@ class ContentEntryViewController: UIViewController {
     
     @IBAction func saveButtonTapped(_ sender: Any) {
         
+        // Code states that the property list is in the xml format.
+        
         var plistFormat = PropertyListSerialization.PropertyListFormat.xml
         
         do {
             
-            let plistData = try Data(contentsOf: documentsDirectoryFileURL()!)
+            // plistData contains the contents of the documents directory which should include Favorites.plist.
             
-            favorite = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as? [String]
+            let plistData = try Data(contentsOf: plistManager.documentsDirectoryFileURL()!)
             
-            if var favorite = favorite {
-                
-                for _ in favorite {
-                    print("Favorite Food: \(String(describing: favoriteFoodTextView.text)), Favorite Movie: \(String(describing: favoriteMovieTextView.text)), Favorite Book: \(String(describing: favoriteBookTextView.text))")
-                }
-                
+            // Retrieves property list data and casts to dictionary with String and Any Key-Value pairs.
+            
+            if var favorites = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as? [String: Any] {
+            
                 let favFood = favoriteFoodTextView.text as String
                 let favMovie = favoriteMovieTextView.text as String
                 let favBook = favoriteBookTextView.text as String
                 
-                favorite.append(favFood)
-                favorite.append(favMovie)
-                favorite.append(favBook)
+                // Appends strings to favorites array in property list. 
                 
-                let serializedData = try PropertyListSerialization.data(fromPropertyList: favorite, format: PropertyListSerialization.PropertyListFormat.xml, options: 0)
+                favorites["favFood"] = favFood
+                favorites["favMovie"] = favMovie
+                favorites["favBook"] = favBook
                 
-                if let file = documentsDirectoryFileURL() {
+                for _ in favorites {
+                    print("Favorite Food: \(String(describing: favFood)), Favorite Movie: \(String(describing: favMovie)), Favorite Book: \(String(describing: favBook))")
+                } 
+                
+                // Turns favorites array into data object containing the property list and the specified format of the property list, which in this case is XML.
+                
+                let serializedData = try PropertyListSerialization.data(fromPropertyList: favorites, format: PropertyListSerialization.PropertyListFormat.xml, options: 0)
+                
+                if let file = plistManager.documentsDirectoryFileURL() {
                     
                     try serializedData.write(to: file)
                     print("The file path is \(file).")
                 }
             }
-        } catch {
+        
+        } catch { 
             
             print("Error")
         }
+        
+        showButton.isEnabled = true 
     }
     
     override func viewDidLoad() {
@@ -73,72 +81,20 @@ class ContentEntryViewController: UIViewController {
         
         // Conditional checks on each app launch whether the plist file exists at the documents directory and copies the file data from the application bundle to the documents directory if that is not the case.
         
-        if fileExistsInDocumentsDirectory() == false {
+        // In order for this to work on each app launch the app must be removed from the project's simulator and the project cleaned.
+        
+        if plistManager.fileExistsInDocumentsDirectory() == false {
             
-            seedDataToDocumentsDirectory()
+            plistManager.seedDataToDocumentsDirectory()
+            print("Favorites.plist has been added to the documents directory.")
         }
+        
+        showButton.isEnabled = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-// Files included in the application bundle are read only, so in order to be able to add data to them, they first must be copied to the app's documents directory.
-
-// On app launch, we want to check if the file in the application bundle has been copied to the documents directory or not. If it has been copied before, we won't do anything. If not, then we'll copy it before attempting to write any new data.
-
-// documentsDirectoryFileURL returns the path for the documents directory we should be writing our plist file to and reading it from.
-
-func documentsDirectoryFileURL() -> URL? {
-    
-    do {
-        
-        let document = try fileManager.url(for: FileManager.SearchPathDirectory.documentDirectory, in: FileManager.SearchPathDomainMask.userDomainMask, appropriateFor: nil, create: false)
-        
-        let file = document.appendingPathComponent("Favorites.plist")
-        
-        return file
-        
-    } catch {
-        
-        print("Error getting file path.")
-        
-        return nil
-    }
-}
-
-    // fileExistsInDocumentsDirectory checks if the file at the path returned by documentsDirectoryFileURL exists or not, and returns a boolean.
-
-    func fileExistsInDocumentsDirectory() -> Bool {
-        
-        if let file = documentsDirectoryFileURL() {
-            
-            let fileExists = FileManager().fileExists(atPath: file.path)
-            
-            return fileExists
-        }
-        
-        return false
-    }
-
-    // seedDataToDocumentsDirectory copies the data from the file stored in the application bundle and writes it to the documents directory at the path provided by documentsDirectoryFileURL.
-
-    func seedDataToDocumentsDirectory() {
-        
-        do {
-            
-            let plistData = try Data(contentsOf: plistUrl!)
-            
-            if let file = documentsDirectoryFileURL() {
-                
-                try plistData.write(to: file)
-            }
-            
-        } catch {
-            
-            print("Error writing file.")
-        }
     }
 } 
 
--- PropertyListTask/SavedContentViewController.swift
+++ PropertyListTask/SavedContentViewController.swift
@@ -9,13 +9,56 @@
 import UIKit
 
 class SavedContentViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    let plistManager = PlistManager()
+    
+    @IBOutlet weak var savedFoodTextView: UITextView!
+    
+    @IBOutlet weak var savedMovieTextView: UITextView!
+  
+    @IBOutlet weak var savedBookTextView: UITextView!
+    
+    // Function that will be called in the viewDidLoad to set the properties for the class according to the property list.
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    func readPropertyList() {
+        
+        // Code states that the property list is in the xml format.
+        
+        var plistFormat = PropertyListSerialization.PropertyListFormat.xml
+        
+        do {
+            
+            // plistData contains the contents of the documents directory which should include Favorites.plist.
+            
+            let plistData = try Data(contentsOf: plistManager.documentsDirectoryFileURL()!)
+            
+            // Retrieves property list data and casts to dictionary with String and Any Key-Value pairs.
+            
+            if var favorites = try PropertyListSerialization.propertyList(from: plistData, options: [], format: &plistFormat) as? [String: Any] {
+                
+                savedFoodTextView.text = favorites["favFood"] as! String
+                savedMovieTextView.text = favorites["favMovie"] as! String
+                savedBookTextView.text = favorites["favBook"] as! String
+            }
+                
+        } catch {
+            
+            print("Error retrieving Favorites.plist favorites array.")
+        }
+        
     }
-
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        readPropertyList()
+    
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -31,5 +74,4 @@ class SavedContentViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
 }
